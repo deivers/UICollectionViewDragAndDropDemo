e476c0291a0c7dc4f47ec697924ed50d4434841c
Doug Ivers <douglasivers@gmail.com>
2014-04-17 22:12:20 -0400

    Add longPressGesture.allowableMovement = 40, fix indentation

--- DragAndDropDemo/SourceViewController.m
+++ DragAndDropDemo/SourceViewController.m
@@ -13,10 +13,10 @@
 #define horizontalInset 30
 
 @interface SourceViewController () <UICollectionViewDataSource, UICollectionViewDelegateFlowLayout> {
-  UICollectionView *_collectionView;
-  ViewController *_parentController;
-  NSMutableArray *_models;
-  MyModel *_selectedModel;
+    UICollectionView *_collectionView;
+    ViewController *_parentController;
+    NSMutableArray *_models;
+    MyModel *_selectedModel;
     int currentPage;
 }
 @end
@@ -25,54 +25,55 @@
 @implementation SourceViewController
 
 - (instancetype)initWithCollectionView:(UICollectionView *)view andParentViewController:(ViewController *)parent {
-  if (self = [super init]) {
-    [self setUpModels];
-    [self initCollectionView:view];
-    [self setUpGestures];
-      currentPage = 0;
-    _parentController = parent;
-  }
-  return self;
+    if (self = [super init]) {
+        [self setUpModels];
+        [self initCollectionView:view];
+        [self setUpGestures];
+        currentPage = 0;
+        _parentController = parent;
+    }
+    return self;
 }
 
 - (void)cellDragCompleteWithModel:(MyModel *)model withValidDropPoint:(BOOL)validDropPoint {
-  if (model != nil) {
-    // get indexPath for the model
-    NSUInteger index = [_models indexOfObject:model];
-    NSIndexPath *indexPath = [NSIndexPath indexPathForItem:index inSection:0];
-    
-    if (validDropPoint && indexPath != nil) {
-      [_models removeObjectAtIndex:index];
-      [_collectionView deleteItemsAtIndexPaths:@[indexPath]];
-      
-      [_collectionView reloadData];
-    } else {
-      
-      UICollectionViewCell *cell = [_collectionView cellForItemAtIndexPath:indexPath];
-      cell.alpha = 1.0f;
+    if (model != nil) {
+        // get indexPath for the model
+        NSUInteger index = [_models indexOfObject:model];
+        NSIndexPath *indexPath = [NSIndexPath indexPathForItem:index inSection:0];
+
+        if (validDropPoint && indexPath != nil) {
+            [_models removeObjectAtIndex:index];
+            [_collectionView deleteItemsAtIndexPaths:@[indexPath]];
+
+            [_collectionView reloadData];
+        } else {
+
+            UICollectionViewCell *cell = [_collectionView cellForItemAtIndexPath:indexPath];
+            cell.alpha = 1.0f;
+        }
     }
-  }
 }
 
 #pragma mark - Setup methods
 - (void)setUpModels {
-  _models = [NSMutableArray array];
-  
-  for (int i=0; i<10; i++) {
-    [_models addObject:[[MyModel alloc] initWithValue:i]];
-  }
+    _models = [NSMutableArray array];
+
+    for (int i=0; i<10; i++) {
+        [_models addObject:[[MyModel alloc] initWithValue:i]];
+    }
 }
 
 - (void)initCollectionView:(UICollectionView *)view {
-  _collectionView            = view;
-  _collectionView.delegate   = self;
-  _collectionView.dataSource = self;
+    _collectionView            = view;
+    _collectionView.delegate   = self;
+    _collectionView.dataSource = self;
     _collectionView.contentInset = UIEdgeInsetsMake(0, horizontalInset, 0, horizontalInset);
-  [_collectionView registerClass:[CardCell class] forCellWithReuseIdentifier:CELL_REUSE_ID];
+    [_collectionView registerClass:[CardCell class] forCellWithReuseIdentifier:CELL_REUSE_ID];
 }
 
 -(void)scrollViewWillEndDragging:(UIScrollView *)scrollView withVelocity:(CGPoint)velocity targetContentOffset:(inout CGPoint *)targetContentOffset {
     currentPage = round(scrollView.contentOffset.x / 270);
+    NSLog(@"%i   %f", currentPage, velocity.x);
     if (velocity.x > 0)
         currentPage++;
     else
@@ -81,56 +82,58 @@
         currentPage = 0;
     else if (currentPage >= [_models count])
         currentPage = [_models count]-1;
+    NSLog(@"%i", currentPage);
     CGFloat hOffset = 240 + 270*(currentPage-1);        ////////////todo: handle landscape and ipad
     *targetContentOffset = CGPointMake(hOffset,0);
 }
 
 - (void)setUpGestures {
-  
-  UILongPressGestureRecognizer *longPressGesture = [[UILongPressGestureRecognizer alloc] initWithTarget:self
-                                                                                                 action:@selector(handlePress:)];
-  longPressGesture.numberOfTouchesRequired = 1;
-  longPressGesture.minimumPressDuration    = 0.1f;
-  [_collectionView addGestureRecognizer:longPressGesture];
+
+    UILongPressGestureRecognizer *longPressGesture = [[UILongPressGestureRecognizer alloc] initWithTarget:self
+                                                                                                   action:@selector(handlePress:)];
+    longPressGesture.numberOfTouchesRequired = 1;
+    longPressGesture.minimumPressDuration = 0.1f;
+    longPressGesture.allowableMovement = 40;
+    [_collectionView addGestureRecognizer:longPressGesture];
 }
 
 #pragma mark - Gesture Recognizer
 - (void)handlePress:(UILongPressGestureRecognizer *)gesture {
-  CGPoint point = [gesture locationInView:_collectionView];
- 
-  if (gesture.state == UIGestureRecognizerStateBegan) {
-    NSIndexPath *indexPath = [_collectionView indexPathForItemAtPoint:point];
-    if (indexPath != nil) {
-      _selectedModel = [_models objectAtIndex:indexPath.item];
-      
-      // calculate point in parent view
-      point = [gesture locationInView:_parentController.view];
-      
-      [_parentController setSelectedModel:_selectedModel atPoint:point];
-      
-      // hide the cell
-      [_collectionView cellForItemAtIndexPath:indexPath].alpha = 0.0f;
+    CGPoint point = [gesture locationInView:_collectionView];
+
+    if (gesture.state == UIGestureRecognizerStateBegan) {
+        NSIndexPath *indexPath = [_collectionView indexPathForItemAtPoint:point];
+        if (indexPath != nil) {
+            _selectedModel = [_models objectAtIndex:indexPath.item];
+
+            // calculate point in parent view
+            point = [gesture locationInView:_parentController.view];
+
+            [_parentController setSelectedModel:_selectedModel atPoint:point];
+
+            // hide the cell
+            [_collectionView cellForItemAtIndexPath:indexPath].alpha = 0.0f;
+        }
     }
-  }
 }
 
 #pragma mark - UICollectionViewDataSource
 - (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
-  return _models.count;
+    return _models.count;
 }
 
 - (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath {
-  CardCell *cell = [_collectionView dequeueReusableCellWithReuseIdentifier:CELL_REUSE_ID forIndexPath:indexPath];
-  
-  MyModel *model = [_models objectAtIndex:indexPath.item];
-  [cell setModel:model];
-  
-  return cell;
+    CardCell *cell = [_collectionView dequeueReusableCellWithReuseIdentifier:CELL_REUSE_ID forIndexPath:indexPath];
+
+    MyModel *model = [_models objectAtIndex:indexPath.item];
+    [cell setModel:model];
+
+    return cell;
 }
 
 #pragma mark - UICollectionViewDelegateFlowLayout
 - (CGSize)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout *)collectionViewLayout sizeForItemAtIndexPath:(NSIndexPath *)indexPath {
-  return CGSizeMake(draggableCellWidth, 50);
+    return CGSizeMake(draggableCellWidth, 50);
 }
 
 //- (UIEdgeInsets)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout *)collectionViewLayout insetForSectionAtIndex:(NSInteger)section {
